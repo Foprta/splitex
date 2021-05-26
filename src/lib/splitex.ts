66e60e6c7ffff0a354b2141c254e81a93275e1f0
Evgeny Klinnikov <mostransformer@gmail.com>
2021-05-26 22:23:15 +0300

    library with expenses pesonal settings

--- src/lib/splitex.ts
+++ src/lib/splitex.ts
@@ -1,4 +1,4 @@
-import { last, first, isEmpty } from "lodash";
+import { first, isEmpty, last } from "lodash";
 
 export interface IFirestoreEntity {
   id: string;
@@ -7,6 +7,13 @@ export interface IFirestoreEntity {
 export interface IUser extends IFirestoreEntity {
   name: string;
   proportion: number;
+}
+
+export interface IExpenseSettings extends IFirestoreEntity {
+  expenseId: string;
+  userId: string;
+  proportion?: number;
+  personal?: number;
 }
 
 export interface IExpense extends IFirestoreEntity {
@@ -24,7 +31,8 @@ export type IManualTransaction = ITransaction & IFirestoreEntity;
 
 export class Splitex {
   users: IUser[] = []; // Юзеры группы
-  expenses: IExpense[] = []; // Траты группы
+  expenses: IExpense[] = []; // Траты
+  expensesSettings: IExpenseSettings[] = []; // Траты группы
 
   totalCost = 0; // Сколько всего было потрачено
   averageExpense = 0; // Траты на одного человека
@@ -43,7 +51,8 @@ export class Splitex {
   constructor(
     users: IUser[],
     expenses: IExpense[],
-    transactions?: IManualTransaction[]
+    manualTransactions: IManualTransaction[] = [],
+    expensesSettings: IExpenseSettings[] = []
   ) {
     if (users.length < 1 || expenses.length < 1) {
       return;
@@ -51,13 +60,12 @@ export class Splitex {
 
     this.users = users;
     this.expenses = expenses;
-    this.manualTransactions = transactions || [];
+    this.manualTransactions = manualTransactions;
+    this.expensesSettings = expensesSettings;
 
     this.totalCost = expenses.reduce((acc, { amount }) => acc + amount, 0);
 
-    this.averageExpense =
-      this.totalCost /
-      users.reduce((acc, { proportion }) => acc + proportion, 0);
+    this.averageExpense = this.totalCost / users.reduce((acc, { proportion }) => acc + proportion, 0);
 
     this.calculateUsersDeposits();
 
@@ -72,6 +80,8 @@ export class Splitex {
     let sortedDeposits = this.mapDepositsIntoArray(bufferedDeposits);
 
     while (sortedDeposits.length >= 2) {
+      if (this.transactions.length > this.users.length) break;
+
       const [toUserId, toAmount] = first(sortedDeposits) as [string, number];
       const [fromUserId, fromAmount] = last(sortedDeposits) as [string, number];
 
@@ -86,9 +96,7 @@ export class Splitex {
     }
   }
 
-  private mapDepositsIntoArray(
-    deposits: Record<string, number>
-  ): Array<[string, number]> {
+  private mapDepositsIntoArray(deposits: Record<string, number>): Array<[string, number]> {
     return Object.entries(deposits)
       .filter(([, amount]) => amount !== 0)
       .sort((a, b) => b[1] - a[1]);
@@ -96,10 +104,8 @@ export class Splitex {
 
   private calculateUsersDeposits(): void {
     this.users.forEach(
-      ({ id, proportion }) =>
-        (this.usersDeposits[id] =
-          this.calculateTotalUserExpenses(id) -
-          this.averageExpense * proportion)
+      (user) =>
+        (this.usersDeposits[user.id] = this.calculateTotalUserExpenses(user.id) - this.calculateTotalUserConsumes(user))
     );
 
     this.manualTransactions.forEach(({ fromUserId, toUserId, amount }) => {
@@ -108,9 +114,34 @@ export class Splitex {
     });
   }
 
+  private calculateTotalUserConsumes(user: IUser): number {
+    return this.expenses.reduce((acc, expense) => {
+      const allProportions = this.users.reduce(
+        (acc, { proportion, id }) =>
+          acc +
+          proportion *
+            (this.expensesSettings.find(({ expenseId, userId }) => expenseId === expense.id && userId === id)
+              ?.proportion ?? 1),
+        0
+      );
+
+      const expenseAmount = this.expensesSettings
+        .filter((settings) => settings.expenseId === expense.id)
+        .reduce((acc, { personal }) => acc - (personal ?? 0), expense.amount);
+
+      const settings = this.expensesSettings.find(
+        ({ expenseId, userId }) => expenseId === expense.id && userId === user.id
+      );
+
+      const userPersonal =
+        this.expensesSettings.find(({ userId, expenseId }) => user.id === userId && expense.id === expenseId)
+          ?.personal ?? 0;
+
+      return userPersonal + acc + (expenseAmount / allProportions) * (settings?.proportion ?? 1) * user.proportion;
+    }, 0);
+  }
+
   private calculateTotalUserExpenses(userId: string): number {
-    return this.expenses
-      .filter((expence) => userId === expence.userId)
-      .reduce((acc, { amount }) => acc + amount, 0);
+    return this.expenses.filter((expence) => userId === expence.userId).reduce((acc, { amount }) => acc + amount, 0);
   }
 }
